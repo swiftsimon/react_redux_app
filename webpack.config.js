7e44ce20023827d45920218f6987e3bdf7a76b99
Simon <sisihamilton@hotmail.com>
2018-08-11 11:00:53 -0400

    bundle, babel, index

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require('path')
-
+// you can use create react app which has its own web pack config
 module.exports = {
     mode:'development',
     entry: {
         app: path.resolve(__dirname, 'src')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: "/"
+    },
+    resolve: {
+        extensions:['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader'
+        }]
     }
 }
